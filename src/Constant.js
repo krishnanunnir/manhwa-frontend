b35bf37b886112e35d9f0dcf1fb00cd554df4546
Krishnanunni R <krishnanunnir97@gmail.com>
2021-12-18 19:25:15 +0530

    minor issue with request going through fix

--- src/Constant.js
+++ src/Constant.js
@@ -1,1 +1,1 @@
-export const baseUrl =process.env.IS_DEV==='true'?'http://127.0.0.1:8080':'http://159.89.82.215';
+export const baseUrl =process.env.IS_DEV==='true'?'http://127.0.0.1:8080':'https://159.89.82.215';
