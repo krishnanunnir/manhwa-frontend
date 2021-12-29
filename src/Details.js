af2ad42cdf8ff42254318818687d4be15202c03d
Krishnanunni R <krishnanunnir97@gmail.com>
2021-12-29 20:43:00 +0530

    Switching to tab 4 indentation for details

--- src/Details.js
+++ src/Details.js
@@ -3,57 +3,62 @@ import { render } from "react-dom";
 import Manhwa from "./components/Manhwa";
 import Disqus from "disqus-react";
 import axios from "axios";
-import {baseUrl} from "./Constant"
-import {Helmet} from "react-helmet";
+import { baseUrl } from "./Constant";
+import { Helmet } from "react-helmet";
 
 class Details extends Component {
-  constructor(props) {
-    super(props);
-    const {
-      params: { manhwaSlug },
-    } = props.match;
-    this.manhwaSlug = manhwaSlug;
-    this.state = {
-      manhwa: {
-        tags: [],
-      },
-    };
-  }
-  componentDidMount() {
-    this.refreshList();
-  }
+	constructor(props) {
+		super(props);
+		const {
+			params: { manhwaSlug },
+		} = props.match;
+		this.manhwaSlug = manhwaSlug;
+		this.state = {
+			manhwa: {
+				tags: [],
+			},
+		};
+	}
+	componentDidMount() {
+		this.refreshList();
+	}
 
-  refreshList = () => {
-    axios
-      .get(`${baseUrl}/api/manhwa/${this.manhwaSlug}/`)
-      .then((res) => this.setState({ manhwa: res.data })).catch((error) =>console.log(error));;
-  };
+	refreshList = () => {
+		axios
+			.get(`${baseUrl}/api/manhwa/${this.manhwaSlug}/`)
+			.then((res) => this.setState({ manhwa: res.data }))
+			.catch((error) => console.log(error));
+	};
 
-  render() {
-	  this.manhwaTitle = this.state.manhwa.title;
-    return (
-      <main className="details container">
-        <Helmet>
-                <meta charSet="utf-8" />
-				<title>{this.manhwaTitle + " | Manre"}</title>
-				<meta name="description" content={"Find out more about '" + this.manhwaTitle +" manhwa"}/>‍
-        </Helmet>
+	render() {
+		this.manhwaTitle = this.state.manhwa.title;
+		return (
+			<main className="details container">
+				<Helmet>
+					<meta charSet="utf-8" />
+					<title>{this.manhwaTitle + " | Manre"}</title>
+					<meta
+						name="description"
+						content={"Find out more about '" + this.manhwaTitle + " manhwa"}
+					/>
+					‍
+				</Helmet>
 
-        <Manhwa item={this.state.manhwa} detailsPage={true} />
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-md-offset-3">
-            <Disqus.DiscussionEmbed
-              shortname={"manhwarec"}
-              config={{
-                url: window.location.href,
-                identifier: this.state.manhwa.id,
-                title: this.state.manhwa.title,
-              }}
-            />
-          </div>
-        </div>
-      </main>
-    );
-  }
+				<Manhwa item={this.state.manhwa} detailsPage={true} />
+				<div className="row justify-content-center">
+					<div className="col-md-6 col-md-offset-3">
+						<Disqus.DiscussionEmbed
+							shortname={"manhwarec"}
+							config={{
+								url: window.location.href,
+								identifier: this.state.manhwa.id,
+								title: this.state.manhwa.title,
+							}}
+						/>
+					</div>
+				</div>
+			</main>
+		);
+	}
 }
 export default Details;
